d02e28df3c401f4ef9a9d6c77bc6dc3a54739f50
alexgoller <83632450+alexgoller@users.noreply.github.com>
2021-05-20 09:19:12 +0200

    Create illumio-core-aliases.sh
    Initial commit

---
+++ illumio-core-aliases.sh
@@ -0,0 +1,6 @@
+# Basic aliases for easier usage on PCE console
+
+alias ilctl="sudo -u ilo-pce /opt/illumio-pce/illumio-pce-ctl"
+alias ilenv="sudo -u ilo-pce /opt/illumio-pce/illumio-pce-env"
+alias ilcheck="sudo -u ilo-pce /opt/illumio-pce/illumio-pce-env check"
+alias ilctl="sudo -u ilo-pce /opt/illumio-pce/illumio-pce-db-management"
