66e0af7850f7255662e4d40692c57df4133d2518
Alexander Goller <alex.goller@illumio.com>
2021-05-28 10:33:22 +0200

    Added more alias
    * ilstatus
    * ilsvw

--- illumio-core-aliases.sh
+++ illumio-core-aliases.sh
@@ -4,3 +4,8 @@ alias ilctl="sudo -u ilo-pce /opt/illumio-pce/illumio-pce-ctl"
 alias ilenv="sudo -u ilo-pce /opt/illumio-pce/illumio-pce-env"
 alias ilcheck="sudo -u ilo-pce /opt/illumio-pce/illumio-pce-env check"
 alias ilctl="sudo -u ilo-pce /opt/illumio-pce/illumio-pce-db-management"
+alias ilstatus="sudo -u ilo-pce /opt/illumio-pce/illumio-pce-ctl status"
+alias ilsvw="sudo -u ilo-pce /opt/illumio-pce/illumio-pce-ctl status -svw"
+alias ilcluster="sudo -u ilo-pce /opt/illumio-pce/illumio-pce-ctl cluster-status"
+
+
